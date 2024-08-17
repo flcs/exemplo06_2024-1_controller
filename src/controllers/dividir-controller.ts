f9ababfa41020023ceb67e219633ed07b0d20c04
Fernando Carvalho <f.carvalho@gsuite.iff.edu.br>
2024-08-17 07:45:25 -0300

    ultimos testes

--- src/controllers/dividir-controller.ts
+++ src/controllers/dividir-controller.ts
@@ -1,16 +1,26 @@
 import { Request, Response } from "express";
 import { IEntrada, ISaida } from "services/dividir-service";
 
-interface IUseCase {
+
+export interface IUseCase {
     perform(entrada: IEntrada): ISaida | Error;
 }
 
 export class DividirController {
     constructor(private readonly useCase: IUseCase) {}
 
-    public async handle(req: Request, res: Response): Promise<void> {
+    public async handler(req: Request, res: Response): Promise<void> {
         const { num1, num2 } = req.body;
 
+        if(num1 === undefined || num2 === undefined ){
+            res.status(400).json({ message : "Faltando paramentros necessarios" })
+            return;
+        }
+        if(typeof(num1) !== "number" || typeof(num2) !== "number"  ){
+            res.status(400).json({ message : "Parametros inválidos" })
+            return;
+        }
+            
         try {
             const entrada = { num1, num2 };
             const saida = this.useCase.perform(entrada);
@@ -18,7 +28,7 @@ export class DividirController {
             res.status(200).json(saida);
         } catch (error) {
             const message = error instanceof Error ? error.message : "Erro desconhecido.";
-            res.status(400).json({ message });
+            res.status(500).json({ message });
         }
     }
 }
--- test/dividir.test.ts
+++ test/dividir.test.ts
@@ -1,4 +1,142 @@
-it('should return 2 when sum 1 + 1', () => {
-  const result = 1 + 1;
-  expect(result).toBe(2);
+import { ISaida } from "services/dividir-service";
+import { DividirController } from "../src/controllers/dividir-controller";
+import { Request, Response } from "express";
+import request from "supertest";
+
+describe("DividirController", () => {
+  test("should return 1 when receves 1 and 1", async () => {
+    // Arrange (configuração)
+    const expectativa: ISaida = { resultado: 1 };
+    const useCaseMock = {
+      perform: jest.fn().mockReturnValue(expectativa),
+    };
+
+    const sut = new DividirController(useCaseMock);
+
+    const requestMock = {
+      body: { num1: 1, num2: 1 },
+    } as unknown as Request;
+
+    const responseMock = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+
+    // Act
+    await sut.handler(requestMock, responseMock);
+
+    // Assert
+    expect(useCaseMock.perform).toHaveBeenCalledWith({ num1: 1, num2: 1 });
+    expect(responseMock.status).toHaveBeenCalledWith(200);
+    expect(responseMock.json).toHaveBeenCalledWith({ resultado: 1 });
+  });
+
+  test("with only one parameter should return 0", async () => {
+    // Arrange
+    const expectativa: ISaida = { resultado: 0 };
+    const useCaseMock = {
+      perform: jest.fn().mockReturnValue(expectativa),
+    };
+
+    const sut = new DividirController(useCaseMock);
+
+    const requestMock = {
+      body: { num1: 1 },
+    } as unknown as Request;
+
+    const responseMock = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+
+    // Act
+    await sut.handler(requestMock, responseMock);
+
+    // Assert
+    expect(useCaseMock.perform).not.toHaveBeenCalled();
+    expect(useCaseMock.perform).toHaveBeenCalledTimes(0);
+    expect(responseMock.status).toHaveBeenCalledWith(400);
+    expect(responseMock.json).toHaveBeenCalledWith({
+      message : "Faltando paramentros necessarios"
+    });
+  })
+
+  test("with zero as second parameter should return an error message", async () => {
+    // Arrange
+    const useCaseMock = {
+      perform: jest.fn().mockImplementation(() => {
+        throw new Error("Divisão por zero não é permitida.");
+      }),
+    };
+
+    const sut = new DividirController(useCaseMock);
+
+    const requestMock = {
+      body: { num1: 1, num2: 0 },
+    } as unknown as Request;
+
+    const responseMock = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+
+    // Act
+    await sut.handler(requestMock, responseMock);
+
+    // Assert
+    expect(useCaseMock.perform).toHaveBeenCalledWith({ num1: 1, num2: 0 });
+    expect(responseMock.status).toHaveBeenCalledWith(500);
+    expect(responseMock.json).toHaveBeenCalledWith({ message: "Divisão por zero não é permitida." });
+  })
+
+  test("with zero as second parameter should return an error message", async () => {
+    // Arrange
+    const useCaseMock = {
+      perform: jest.fn().mockImplementation(() => {
+        
+      }),
+    };
+
+    const sut = new DividirController(useCaseMock);
+
+    const requestMock = {
+      body: { num1: "1", num2: "2" },
+    } as unknown as Request;
+
+    const responseMock = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+
+    // Act
+    await sut.handler(requestMock, responseMock);
+
+    // Assert
+    expect(useCaseMock.perform).toHaveBeenCalledTimes(0);
+    expect(responseMock.status).toHaveBeenCalledWith(400);
+    expect(responseMock.json).toHaveBeenCalledWith({ message: "Parametros inválidos" });
+  })
+
+  // base para novos testes
+  // test('', ()=>{
+  //   // Arrange
+  //   const expectativa: ISaida = { resultado: 0 };
+  //   const useCaseMock = {
+  //     perform: jest.fn().mockReturnValue(expectativa),
+  //   };
+  //   // Supertest
+  //   const sut = new DividirController(useCaseMock);
+  //   const requestMock = {
+  //     body: { num1: 1, num2: 0 },
+  //   } as unknown as Request;
+  //   const responseMock = {
+  //     status: jest.fn().mockReturnThis(),
+  //     json: jest.fn(),
+  //   } as unknown as Response;
+  //   // Act
+  //   const retorno = sut.handler(requestMock, responseMock);
+  //   // Assert
+  //   expect(useCaseMock.perform).toHaveBeenCalledWith({ num1: 1, num2: 0 });
+  // })
+  
 });
